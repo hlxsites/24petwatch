0d43d6a3be06f2acdf499f11b98ef53d25d8f053
Aditya Chabuku <146018498+chabukua-ipg@users.noreply.github.com>
2024-01-15 10:33:03 -0500

    fixed eslint issues
     https://feature-embed-video-blocks--24petwatch--hlxsites.hlx.page/

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -1,4 +1,3 @@
-
 /*
  * Embed Block
  * Show videos and social posts directly on your page
@@ -6,190 +5,208 @@
  */
 
 const loadScript = (url, callback, type) => {
-    const head = document.querySelector('head');
-    const script = document.createElement('script');
-    script.src = url;
-    if (type) {
-      script.setAttribute('type', type);
-    }
-    script.onload = callback;
-    head.append(script);
-    return script;
-  };
-  
-  const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  script.src = url;
+  if (type) {
+    script.setAttribute('type', type);
+  }
+  script.onload = callback;
+  head.append(script);
+  return script;
+};
+
+const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
       <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
         scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
       </iframe>
     </div>`;
-  
-  const embedYoutube = (url, autoplay) => {
-    const usp = new URLSearchParams(url.search);
-    const suffix = autoplay ? '&muted=1&autoplay=1' : '';
-    let vid = usp.get('v') ? encodeURIComponent(usp.get('v')) : '';
-    const embed = url.pathname;
-    if (url.origin.includes('youtu.be')) {
-      [, vid] = url.pathname.split('/');
-    }
-    const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+
+const embedYoutube = (url, autoplay) => {
+  const usp = new URLSearchParams(url.search);
+  const suffix = autoplay ? '&muted=1&autoplay=1' : '';
+  let vid = usp.get('v') ? encodeURIComponent(usp.get('v')) : '';
+  const embed = url.pathname;
+  if (url.origin.includes('youtu.be')) {
+    [, vid] = url.pathname.split('/');
+  }
+  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
         <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
         allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
       </div>`;
-    return embedHTML;
-  };
-  
-  const embedVimeo = (url, autoplay) => {
-    const [, video] = url.pathname.split('/');
-    const suffix = autoplay ? '?muted=1&autoplay=1' : '';
-    const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+  return embedHTML;
+};
+
+const embedVimeo = (url, autoplay) => {
+  const [, video] = url.pathname.split('/');
+  const suffix = autoplay ? '?muted=1&autoplay=1' : '';
+  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
         <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
         style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
         frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
         title="Content from Vimeo" loading="lazy"></iframe>
       </div>`;
-    return embedHTML;
-  };
-  
-  const embedTwitter = (url) => {
-    const embedHTML = `<blockquote class="twitter-tweet"><a href="${url.href}"></a></blockquote>`;
-    loadScript('https://platform.twitter.com/widgets.js');
-    return embedHTML;
-  };
+  return embedHTML;
+};
 
-  const embedSurveryMonky =(jsScript,className) =>{
-    
-    try{
-        const embedHTML = smScript(jsScript,className);
-        return embedHTML;
-    }catch (error) {
-        console.error('Error executing script:', error);
-      }
-   
+const embedTwitter = (url) => {
+  const embedHTML = `<blockquote class="twitter-tweet"><a href="${url.href}"></a></blockquote>`;
+  loadScript('https://platform.twitter.com/widgets.js');
+  return embedHTML;
+};
 
-  }
-  function smScript(jsUrl,className){
-   
-(function(t,e,s,n){ 
-    var o,a,c; 
-   // var className = 'embed-surveymonkey';
-    t.SMCX=t.SMCX||[],
-    e.getElementById(n)||
-    (o=e.getElementsByTagName(s),
-    a=o[o.length-1], 
-    
-    c=e.createElement(s), 
-    
-    c.type="text/javascript", 
-    
-    c.async = true,
-    
-    c.id=n, 
-    
-    c.src= extractURL(jsUrl.replace(/\s/g, '')), 
-  
-        c.onload = function() {
-        // Find the element with class name "myClass" and append the script to it
-        var myClassElement = e.getElementsByClassName(className)[0];
-        if (myClassElement) {
-            myClassElement.innerHTML="";
-            myClassElement.appendChild(c);
-        }else{
-           
-        }
-
-        },
-       a.parentNode.insertBefore(c,a)
-       // 
-
-   )})(window,document,"script","smcx-sdk"); 
-  
-  }
-
-  function extractURL(htmlInput) {
-    if(htmlInput.startsWith("https://")){
+function extractURL(htmlInput) {
+  if (htmlInput.startsWith('https://')) {
     return htmlInput;
-    }else{
-    // Use regular expressions to find URLs that start with "src=" or "href=" and end with ".js"
-    var jsUrls = htmlInput.match(/(?:src|href)\s*=\s*["']([^"']*\.js)["']/gi);
+  }
+  // Use regular expressions to find URLs that start with "src=" or "href=" and end with ".js"
+  let jsUrls = htmlInput.match(/(?:src|href)\s*=\s*["']([^"']*\.js)["']/gi);
 
-    // Extract URLs from the matches
-    if (jsUrls) {
-        jsUrls = jsUrls.map(function (url) {
-            return url.match(/["']([^"']*)["']/)[1];
-        });
-    }
-    
+  // Extract URLs from the matches
+  if (jsUrls) {
+    jsUrls = jsUrls.map((url) => url.match(/["']([^"']*)["']/)[1]);
+  }
 
-    return jsUrls || [];
-    }
+  return jsUrls || [];
 }
-  
-  const loadEmbed = (block, link, autoplay) => {
-    if (block.classList.contains('embed-is-loaded')) {
-      return;
-    }
-  
-    const EMBEDS_CONFIG = [
-      {
-        match: ['youtube', 'youtu.be'],
-        embed: embedYoutube,
-      },
-      {
-        match: ['vimeo'],
-        embed: embedVimeo,
-      },
-      {
-        match: ['twitter'],
-        embed: embedTwitter,
-      },
-    ];
-  
-    const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
-    const url = new URL(link);
-    if (config) {
-      block.innerHTML = config.embed(url, autoplay);
-      block.classList = `block embed embed-${config.match[0]}`;
-    } else {
-      block.innerHTML = getDefaultEmbed(url);
-      block.classList = 'block embed';
-    }
-    block.classList.add('embed-is-loaded');
-  };
+/*
+function smScript(jsUrl, className) {
+  (function (t, e, s, n) {
+    let o; let a; let c;
+    // var className = 'embed-surveymonkey';
+    t.SMCX = t.SMCX || [],
+    e.getElementById(n)
+    || (o = e.getElementsByTagName(s),
+    a = o[o.length - 1],
 
+    c = e.createElement(s),
 
-  const loadEmbedScript = (block, jsScript) =>{
-    if (block.classList.contains('embed-is-loaded')) {
+    c.type = 'text/javascript',
+
+    c.async = true,
+
+    c.id = n,
+
+    c.src = extractURL(jsUrl.replace(/\s/g, '')),
+
+    c.onload = function () {
+      // Find the element with class name "myClass" and append the script to it
+      const myClassElement = e.getElementsByClassName(className)[0];
+      if (myClassElement) {
+        myClassElement.innerHTML = '';
+        myClassElement.appendChild(c);
+      } else {
         return;
-      }else{
-        var randomNumber =  Math.floor(Math.random() * 900) + 100;
-        var className =`embed-surveymonkey-`+randomNumber;
-      //block.classList = `embed-surveymonkey-`+randomNumber;
-      block.classList.add('block','embed',className); 
+      }
+    },
+    a.parentNode.insertBefore(c, a)
+    //
 
-      block.innerHTML = embedSurveryMonky(jsScript,className );
-      
-      block.classList.add('embed-is-loaded');
+    );
+  }(window, document, 'script', 'smcx-sdk'));
+} */
 
+function smScript(jsUrl, className) {
+  (function initializeSMCX(t, e, s, n) {
+    let o; let a; let
+      c;
+    // var className = 'embed-surveymonkey';
+    t.SMCX = t.SMCX || [];
 
+    if (!e.getElementById(n)) {
+      o = e.getElementsByTagName(s);
+      a = o[o.length - 1];
+      c = e.createElement(s);
+
+      c.type = 'text/javascript';
+      c.async = true;
+      c.id = n;
+      c.src = extractURL(jsUrl.replace(/\s/g, ''));
+
+      c.onload = function () {
+        // Find the element with class name "myClass" and append the script to it
+        const myClassElement = e.getElementsByClassName(className)[0];
+        if (myClassElement) {
+          myClassElement.innerHTML = '';
+          myClassElement.appendChild(c);
+        } else { // no action required.
+        }
+      };
+
+      a.parentNode.insertBefore(c, a);
     }
+  }(window, document, 'script', 'smcx-sdk'));
+}
 
-  };
-  
-  export default function decorate(block) {
+const embedSurveryMonky = (jsScript, className) => {
+  let embedHTML = '';
+  try {
+    embedHTML = smScript(jsScript, className);
+  } catch (error) {
+    // console.error('Error executing script:', error);
+  }
+  return embedHTML;
+};
 
-    if (block.textContent.includes('surveymonkey')) {
-        var jsScript = block.outerHTML.replace(/<div[^>]*>|<\/?p>|&lt;script&gt;|&lt;\/script&gt;/g, '');
-        jsScript =  jsScript.replace(/<\/div>\s*/g, '');
-       // jsScript = jsScript.replace(/[\n\s"]+/g, '');
-            loadEmbedScript(block,jsScript);
-       //     window.setTimeout(() => loadedLazy, 3000);
+const loadEmbed = (block, link, autoplay) => {
+  if (block.classList.contains('embed-is-loaded')) {
+    // no action required.
+  }
 
+  const EMBEDS_CONFIG = [
+    {
+      match: ['youtube', 'youtu.be'],
+      embed: embedYoutube,
+    },
+    {
+      match: ['vimeo'],
+      embed: embedVimeo,
+    },
+    {
+      match: ['twitter'],
+      embed: embedTwitter,
+    },
+  ];
 
-    }else{
-        const placeholder = block.querySelector('picture');
-        const link = block.querySelector('a').href;
-        block.textContent = '';
-  
+  const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
+  const url = new URL(link);
+  if (config) {
+    block.innerHTML = config.embed(url, autoplay);
+    block.classList = `block embed embed-${config.match[0]}`;
+  } else {
+    block.innerHTML = getDefaultEmbed(url);
+    block.classList = 'block embed';
+  }
+  block.classList.add('embed-is-loaded');
+};
+
+const loadEmbedScript = (block, jsScript) => {
+  if (block.classList.contains('embed-is-loaded')) {
+    // no action required.
+  } else {
+    const randomNumber = Math.floor(Math.random() * 900) + 100;
+    const className = `embed-surveymonkey-${randomNumber}`;
+    // block.classList = `embed-surveymonkey-`+randomNumber;
+    block.classList.add('block', 'embed', className);
+
+    block.innerHTML = embedSurveryMonky(jsScript, className);
+
+    block.classList.add('embed-is-loaded');
+  }
+};
+
+export default function decorate(block) {
+  if (block.textContent.includes('surveymonkey')) {
+    let jsScript = block.outerHTML.replace(/<div[^>]*>|<\/?p>|&lt;script&gt;|&lt;\/script&gt;/g, '');
+    jsScript = jsScript.replace(/<\/div>\s*/g, '');
+    // jsScript = jsScript.replace(/[\n\s"]+/g, '');
+    loadEmbedScript(block, jsScript);
+    //     window.setTimeout(() => loadedLazy, 3000);
+  } else {
+    const placeholder = block.querySelector('picture');
+    const link = block.querySelector('a').href;
+    block.textContent = '';
+
     if (placeholder) {
       const wrapper = document.createElement('div');
       wrapper.className = 'embed-placeholder';
@@ -208,8 +225,5 @@ const loadScript = (url, callback, type) => {
       });
       observer.observe(block);
     }
-}
-       
-
-
   }
+}
