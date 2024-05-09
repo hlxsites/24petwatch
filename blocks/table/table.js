d355f7bd5541e6369ba9fe29ba6274e3ff0d519f
Bohdan Korablov <korablov@adobe.com>
2024-05-09 17:34:10 -0500

    #96 Add some styles and icons for pet protection table

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -10,6 +10,47 @@ function buildCell(rowIndex) {
   return cell;
 }
 
+function isHeaderCell(cell) {
+  return cell.nodeName === 'TH';
+}
+
+function isCheckmark(str) {
+  return str === '✓';
+}
+
+function warpStrWithITag(cell, str) {
+  if (!isHeaderCell(cell) && isCheckmark(str)) {
+    return `<i>${str}</i>`;
+  }
+
+  return str;
+}
+
+function addMostPopular(cell, col) {
+  const pElement = col.querySelector('p');
+  if (pElement && isCheckmark(pElement.innerHTML)) {
+    col.removeChild(pElement);
+    cell.classList.add('most-popular');
+  }
+}
+
+function addInfoBlock(cell) {
+  const [coverage, information] = cell.innerHTML.split('---');
+
+  if (information) {
+    let result = '';
+    const divInfoIcon = document.createElement('div');
+    const divInfoContent = document.createElement('div');
+    divInfoIcon.classList.add('info-icon');
+    divInfoContent.classList.add('info-content');
+    divInfoContent.innerHTML = information;
+    divInfoIcon.innerHTML = divInfoContent.outerHTML;
+    result = coverage + divInfoIcon.outerHTML;
+
+    cell.innerHTML = result;
+  }
+}
+
 export default async function decorate(block) {
   const table = document.createElement('table');
   const thead = document.createElement('thead');
@@ -25,9 +66,15 @@ export default async function decorate(block) {
     const row = document.createElement('tr');
     if (header && i === 0) thead.append(row);
     else tbody.append(row);
-    [...child.children].forEach((col) => {
+    [...child.children].forEach((col, j) => {
       const cell = buildCell(header ? i : i + 1);
-      cell.innerHTML = col.innerHTML;
+      if (isHeaderCell(cell)) {
+        addMostPopular(cell, col);
+      }
+      cell.innerHTML = warpStrWithITag(cell, col.innerHTML);
+      if (j === 0) {
+        addInfoBlock(cell);
+      }
       row.append(cell);
     });
   });
