5e4714eac53fcd1ec8c470f3a6e3694820fb47a8
sagar-pethealth <107197116+sagar-pethealth@users.noreply.github.com>
2024-06-21 19:30:24 +0530

    Feature/new mega nav eds ( Reverted changes on main because of image issue ) (#123)
    * #193 - Initial html set up for mega nav
    
    * Added css to show content on hover
    
    * Added top nav with country selector
    
    * Added mobile view css for mega nav
    
    * Added fixes to QA Observations
    
    * Lint issue fixed
    
    * CSS updated as per latest suggestion
    
    * Updated box-shadow for memebership and register menu content
    
    * Added canada links to header anchor tag
    
    * Applied suggestion changes from Ben
    
    * Applied QA suggestions
    
    * Close dropdown content on outside click
    
    * new-mega-nav-eds: reference new nav from a well-known location
    
    * Added code provided by Felix to get images from document based media folder
    
    ---------
    
    Co-authored-by: shiv-pethealth <shiv.alkondwar@pethealthinc.com>
    Co-authored-by: Mike Weis <miweis@magento.com>

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -5,27 +5,30 @@ import {
   decorateLinks,
   isMobile,
   isTablet,
-  isDesktop,
+  // isDesktop,
   isCanada,
-  isLiveSite,
-  isCrosswalkDomain,
-  getXWalkDomain,
+  // isLiveSite,
+  // isCrosswalkDomain,
+  // getXWalkDomain,
+  baseDomain,
 } from '../../scripts/lib-franklin.js';
 import { trackGTMEvent } from '../../scripts/lib-analytics.js';
 
-let positionY = 0;
-const SCROLL_STEP = 25;
+// let positionY = 0;
+// const SCROLL_STEP = 25;
 
 const urls = {
   usa: {
     url: '/',
     name: 'US',
+    imageName: 'flagusa',
     icon: 'icon-flagusa',
     lang: 'en-US',
   },
   canada: {
     url: '/ca',
     name: 'Canada',
+    imageName: 'flagcanada',
     icon: 'icon-flagcanada',
     lang: 'en-CA',
   },
@@ -69,7 +72,7 @@ function focusNavSection() {
  * @param {Boolean} expanded Whether the element should be expanded or collapsed
  */
 function toggleAllNavSections(sections, expanded = false) {
-  sections.querySelectorAll('.nav-sections > ul > li').forEach((section) => {
+  sections.querySelectorAll('#nav > div').forEach((section) => {
     section.setAttribute('aria-expanded', expanded);
   });
 }
@@ -95,7 +98,7 @@ function toggleMenu(nav, navSections, closeAll = null) {
 
   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
   wrapper?.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-  toggleAllNavSections(navSections, expanded);
+  toggleAllNavSections(nav, expanded);
   button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
   // enable nav dropdown keyboard accessibility
   const navDrops = navSections.querySelectorAll('.nav-drop');
@@ -135,14 +138,14 @@ function decorateLanguageSelector(block) {
   if (isCanada) {
     newCountryUrl = new URL(window.location.pathname.replace('/ca', ''), window.location.origin);
   } else {
-    newCountryUrl = new URL(`/ca${window.location.pathname}`, window.location.origin);
+    newCountryUrl = new URL(`/ca${(window.location.pathname === '/') ? '' : window.location.pathname}`, window.location.origin);
   }
 
   const languageSelector = document.createElement('li');
   languageSelector.classList.add('language-selector');
-  languageSelector.innerHTML = `<span class="icon ${currentCountry.icon}"></span>
+  languageSelector.innerHTML = `<span class="icon ${currentCountry.icon}"><img src='/icons/${currentCountry.imageName}.svg' /><span id='country-name'></span></span>
       <ul>
-        <li><a href="${newCountryUrl.toString()}" hreflang="${alternateCountry.lang}" rel="alternate" title="${alternateCountry.name}"><span class="icon ${alternateCountry.icon}"></span>${alternateCountry.name}</a></li>
+        <li><a href="${newCountryUrl.href}" hreflang="${alternateCountry.lang}" rel="alternate" title="${alternateCountry.name}"><span class="icon ${alternateCountry.icon}"><img src='/icons/${alternateCountry.imageName}.svg' /></span>${alternateCountry.name}</a></li>
       </ul>`;
 
   const secondaryMenu = block.querySelector(':scope > ul');
@@ -269,6 +272,34 @@ function addExternalLinkIcons(header) {
 }
 
 /**
+ * Fetch images from the document based media folder.
+ * @param {Element} block The header block element
+ */
+function fetchImagesFromMediaFolder(block) {
+  block.querySelectorAll('source').forEach((source) => {
+    let src;
+    try {
+      src = new URL(source.srcset);
+    } catch (e) {
+      src = new URL(source.srcset, baseDomain);
+    }
+    src.pathname = `/blog${src.pathname}`;
+    source.srcset = `.${src.pathname}${src.search}`;
+  });
+
+  block.querySelectorAll('img').forEach((img) => {
+    let src;
+    try {
+      src = new URL(img.src);
+    } catch (e) {
+      src = new URL(img.src, baseDomain);
+    }
+    src.pathname = `/blog${src.pathname}`;
+    img.src = `.${src.pathname}${src.search}`;
+  });
+}
+
+/**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
@@ -276,110 +307,254 @@ export default async function decorate(block) {
   // fetch nav content
   const navMeta = getMetadata('nav');
   block.textContent = '';
-  let baseHeaderUrl = '/fragments/header/master';
+  let baseHeaderUrl = '/blog/fragments/us/mega-nav';
   if (isCanada) {
-    baseHeaderUrl = '/fragments/ca/header/master';
+    baseHeaderUrl = '/blog/fragments/ca/mega-nav';
   }
 
-  if (!isLiveSite && !isCrosswalkDomain) {
-    if (isCanada) {
-      baseHeaderUrl = `https://${getXWalkDomain()}/fragments/ca/header/master`;
-    } else {
-      baseHeaderUrl = `https://${getXWalkDomain()}/fragments/header/master`;
-    }
-  }
+  // Note: This is commented out because the header is not being served from the crosswalk domain
+  // if (!isLiveSite && !isCrosswalkDomain) {
+  //   if (isCanada) {
+  //     baseHeaderUrl = `https://${getXWalkDomain()}/fragments/ca/header/master`;
+  //   } else {
+  //     baseHeaderUrl = `https://${getXWalkDomain()}/fragments/header/master`;
+  //   }
+  // }
 
   const navPath = navMeta ? new URL(navMeta).pathname : baseHeaderUrl;
   const resp = await fetch(`${navPath}.plain.html`);
 
-  if (resp.ok) {
-    const html = await resp.text();
-
-    // decorate nav DOM
-    const nav = document.createElement('nav');
-    nav.id = 'nav';
-    nav.innerHTML = html;
-
-    const classes = ['brand', 'get-quote', 'sections', 'secondary'];
-    classes.forEach((c, i) => {
-      const section = nav.children[i];
-      if (section) section.classList.add(`nav-${c}`);
-    });
-
-    const navSections = nav.querySelector('.nav-sections');
-    if (navSections) {
-      navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
-        if (navSection.querySelector('ul')) {
-          navSection.classList.add('nav-drop');
-          const dropdownAnchor = document.createElement('a');
-          dropdownAnchor.href = '#';
-          dropdownAnchor.ariaLabel = 'Open dropdown menu';
-          dropdownAnchor.classList.add('icon-arrow');
-          dropdownAnchor.addEventListener('click', (e) => {
-            e.preventDefault();
-          });
-          navSection.append(dropdownAnchor);
-        }
-        navSection.addEventListener('click', (e) => {
-          if (!isTablet.matches && !isDesktop.matches && e.target.tagName === 'A') return;
-
-          const expanded = navSection.getAttribute('aria-expanded') === 'true';
-          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-        });
-        navSection.addEventListener('mouseenter', () => {
-          if (isDesktop.matches) {
-            navSection.setAttribute('aria-expanded', 'true');
-          }
-        });
-        navSection.addEventListener('mouseleave', () => {
-          if (isDesktop.matches) {
-            navSection.setAttribute('aria-expanded', 'false');
-          }
-        });
-      });
-    }
-
-    // hamburger for mobile
-    const hamburger = document.createElement('div');
-    hamburger.classList.add('nav-hamburger');
-    hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
-        <span></span><span></span><span></span>
-      </button>`;
-    hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
-    nav.prepend(hamburger);
-    nav.setAttribute('aria-expanded', 'false');
-    // prevent mobile nav behavior on window resize
-    toggleMenu(nav, navSections, isTablet.matches);
-    isTablet.addEventListener('change', () => closeAllMenus(nav, navSections));
-
-    // language selector
-    const secondaryMenu = nav.querySelector('.nav-secondary');
-    decorateLanguageSelector(secondaryMenu);
-
-    decorateIcons(nav);
-    decorateButtons(nav);
-    decorateLinks(nav);
-    instrumentTrackingEvents(nav);
-    removeTargetBlank(nav);
-    addLinkToLogo(nav);
-    addExternalLinkIcons(nav);
-
-    const navWrapper = document.createElement('div');
-    navWrapper.className = 'nav-wrapper';
-    navWrapper.append(nav);
-    block.append(navWrapper);
-
-    window.addEventListener('scroll', () => {
-      if (window.pageYOffset - positionY > SCROLL_STEP && !navWrapper.classList.contains('slide-up')) {
-        navWrapper.classList.remove('slide-down');
-        navWrapper.classList.add('slide-up');
-      }
-      if (positionY - window.pageYOffset > SCROLL_STEP && !navWrapper.classList.contains('slide-down')) {
-        navWrapper.classList.remove('slide-up');
-        navWrapper.classList.add('slide-down');
-      }
-
-      positionY = window.pageYOffset;
-    }, false);
+  if (!resp.ok) {
+    return;
   }
+
+  const html = await resp.text();
+  // decorate nav DOM
+  const nav = document.createElement('nav');
+  nav.id = 'nav';
+  nav.innerHTML = html;
+
+  const classes = ['brand', 'meganav', 'memberships', 'register', 'secondary'];
+  classes.forEach((c, i) => {
+    const section = nav.children[i];
+    if (section) section.classList.add(`nav-${c}`);
+  });
+
+  const navWrapper = document.createElement('div');
+  navWrapper.className = 'nav-wrapper';
+  navWrapper.append(nav);
+  block.append(navWrapper);
+  addCanadaToLinks(navWrapper);
+
+  // Append membership hover content
+  const membershipsHoverContent = nav.querySelector('.nav-memberships');
+  navWrapper.append(membershipsHoverContent);
+
+  // Append register hover content
+  const registerHoverContent = nav.querySelector('.nav-register');
+  navWrapper.append(registerHoverContent);
+  decorateLinks(registerHoverContent);
+
+  // Add icon to login links
+  const loginLinks = nav.querySelector('.login');
+  addExternalLinkIcons(loginLinks);
+
+  decorateIcons(nav);
+  decorateButtons(nav);
+  decorateLinks(nav);
+  instrumentTrackingEvents(nav);
+  removeTargetBlank(nav);
+  addLinkToLogo(nav);
+  fetchImagesFromMediaFolder(block);
+
+  // get mega nav elements
+  const megaNav = nav.querySelector('.nav-meganav .mega-nav');
+  if (megaNav) {
+    // add id to each div of mega nav
+    megaNav.querySelectorAll('.mega-nav > div').forEach((div, index) => {
+      div.id = `meganav-link-${index + 1}`;
+    });
+  }
+
+  const navMembershipText = nav.querySelector('#meganav-link-1');
+  const membershipDiv = navMembershipText.querySelector('div');
+  membershipDiv.className = 'before-click';
+  const navRegisterText = nav.querySelector('#meganav-link-4');
+  const registerDiv = navRegisterText.querySelector('div');
+  registerDiv.className = 'before-click';
+  const loginDiv = nav.querySelector('.login > div');
+  const loginText = nav.querySelector('.login > div > div:first-child');
+  const loginHoverContent = nav.querySelector('.login > div > div:last-child');
+
+  // Add click events.
+  navMembershipText.addEventListener('click', () => {
+    if (membershipsHoverContent.style.display === 'flex') {
+      membershipsHoverContent.style.display = 'none';
+      membershipDiv.className = 'before-click';
+    } else {
+      registerHoverContent.style.display = 'none';
+      registerDiv.className = 'before-click';
+      loginText.className = '';
+      if (window.innerWidth <= 900) {
+        loginText.className = 'before-click';
+      }
+      loginHoverContent.style.display = 'none';
+      membershipsHoverContent.style.display = 'flex';
+      membershipDiv.className = 'after-click active';
+    }
+  });
+
+  navRegisterText.addEventListener('click', () => {
+    if (registerHoverContent.style.display === 'flex') {
+      registerHoverContent.style.display = 'none';
+      registerDiv.className = 'before-click';
+    } else {
+      membershipsHoverContent.style.display = 'none';
+      membershipDiv.className = 'before-click';
+      loginText.className = '';
+      if (window.innerWidth <= 900) {
+        loginText.className = 'before-click';
+      }
+      loginHoverContent.style.display = 'none';
+      registerHoverContent.style.display = 'flex';
+      registerDiv.className = 'after-click active';
+    }
+  });
+
+  loginDiv.addEventListener('click', () => {
+    if (loginHoverContent.style.display === 'flex') {
+      loginHoverContent.style.display = 'none';
+      loginText.className = '';
+      if (window.innerWidth <= 900) {
+        loginText.className = 'before-click';
+      }
+    } else {
+      loginHoverContent.style.display = 'flex';
+      loginText.className = 'active';
+      membershipsHoverContent.style.display = 'none';
+      membershipDiv.className = 'before-click';
+      registerHoverContent.style.display = 'none';
+      registerDiv.className = 'before-click';
+      if (window.innerWidth <= 900) {
+        loginText.className = 'after-click active';
+      }
+    }
+  });
+
+  // language selector
+  const secondaryMenu = nav.querySelector('.nav-secondary');
+  decorateLanguageSelector(secondaryMenu);
+
+  const navBrand = nav.querySelector('.nav-brand');
+  const navMega = nav.querySelector('.nav-meganav');
+  const languageSelected = nav.querySelector('.language-selector #country-name');
+  let countryName = isCanada ? 'Canada' : 'United States';
+  if (window.innerWidth <= 900) {
+    navBrand.append(nav.querySelector('.button-container'));
+    navMembershipText.append(membershipsHoverContent);
+    navRegisterText.append(registerHoverContent);
+    loginText.className = 'before-click';
+    languageSelected.append(countryName);
+  }
+  // Do this on resize
+  window.addEventListener('resize', () => {
+    countryName = isCanada ? 'Canada' : 'United States';
+    if (window.innerWidth <= 900) {
+      navBrand.append(nav.querySelector('.button-container'));
+      navMembershipText.append(membershipsHoverContent);
+      navRegisterText.append(registerHoverContent);
+      loginText.className = 'before-click';
+      if (languageSelected.innerHTML === '') {
+        languageSelected.innerHTML = countryName;
+      }
+    } else {
+      navMega.append(nav.querySelector('.button-container'));
+      navWrapper.append(membershipsHoverContent);
+      navWrapper.append(registerHoverContent);
+      loginText.classList.remove('before-click');
+      loginText.classList.remove('after-click');
+      if (languageSelected.innerHTML !== '') {
+        languageSelected.innerHTML = '';
+      }
+    }
+  });
+
+  // const navSections = nav.querySelector('.nav-meganav');
+  // if (navSections) {
+  //   console.log('Inside Nav section', navSections);
+  //   navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
+  //     if (navSection.querySelector('ul')) {
+  //       navSection.classList.add('nav-drop');
+  //       const dropdownAnchor = document.createElement('a');
+  //       dropdownAnchor.href = '#';
+  //       dropdownAnchor.ariaLabel = 'Open dropdown menu';
+  //       dropdownAnchor.classList.add('icon-arrow');
+  //       dropdownAnchor.addEventListener('click', (e) => {
+  //         e.preventDefault();
+  //       });
+  //       navSection.append(dropdownAnchor);
+  //     }
+  //     navSection.addEventListener('click', (e) => {
+  //       if (!isTablet.matches && !isDesktop.matches && e.target.tagName === 'A') return;
+
+  //       const expanded = navSection.getAttribute('aria-expanded') === 'true';
+  //       navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+  //     });
+  //     navSection.addEventListener('mouseenter', () => {
+  //       if (isDesktop.matches) {
+  //         navSection.setAttribute('aria-expanded', 'true');
+  //       }
+  //     });
+  //     navSection.addEventListener('mouseleave', () => {
+  //       if (isDesktop.matches) {
+  //         navSection.setAttribute('aria-expanded', 'false');
+  //       }
+  //     });
+  //   });
+  // }
+
+  // hamburger for mobile
+  const hamburger = document.createElement('div');
+  hamburger.classList.add('nav-hamburger');
+  hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
+      <span></span><span></span><span></span>
+    </button>`;
+  hamburger.addEventListener('click', () => toggleMenu(nav, nav));
+  navBrand.append(hamburger);
+  nav.setAttribute('aria-expanded', 'false');
+  // prevent mobile nav behavior on window resize
+  toggleMenu(nav, navMega, isTablet.matches);
+  isTablet.addEventListener('change', () => closeAllMenus(nav, nav));
+
+  // Close dropdown content on scroll.
+  window.addEventListener('scroll', () => {
+    registerHoverContent.style.display = 'none';
+    registerDiv.className = 'before-click';
+    membershipsHoverContent.style.display = 'none';
+    membershipDiv.className = 'before-click';
+    loginHoverContent.style.display = 'none';
+    loginText.className = '';
+    if (window.innerWidth <= 900) {
+      loginText.className = 'before-click';
+    }
+  }, false);
+
+  // Close dropdown content on outside click.
+  document.addEventListener('click', (e) => {
+    if (!membershipsHoverContent.contains(e.target) && membershipDiv.classList.contains('active') && !navMembershipText.contains(e.target)) {
+      membershipsHoverContent.style.display = 'none';
+      membershipDiv.className = 'before-click';
+    }
+    if (!registerHoverContent.contains(e.target) && registerDiv.classList.contains('active') && !navRegisterText.contains(e.target)) {
+      registerHoverContent.style.display = 'none';
+      registerDiv.className = 'before-click';
+    }
+    if (!loginHoverContent.contains(e.target) && loginText.classList.contains('active') && !loginDiv.contains(e.target)) {
+      loginHoverContent.style.display = 'none';
+      loginText.className = '';
+      if (window.innerWidth <= 900) {
+        loginText.className = 'before-click';
+      }
+    }
+  }, false);
 }
