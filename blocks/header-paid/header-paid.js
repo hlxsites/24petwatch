e50c7a90460472d55a623b08d47764a1b6d2b84d
Daniel O'Kane <daniel.okane@herodigital.com>
2024-09-13 13:00:11 -0400

    Datalayer updates for feature/membership-v2-paid-page

--- blocks/header-paid/header-paid.js
+++ blocks/header-paid/header-paid.js
@@ -1,6 +1,30 @@
 import { jsx, changeDomain, addCanadaToLinks } from '../../scripts/scripts.js';
 import { loadFragment } from '../fragment/fragment.js';
 import { isCanada, decorateLinks } from '../../scripts/lib-franklin.js';
+import { trackGTMEvent } from '../../scripts/lib-analytics.js';
+
+/**
+ * instruments the tracking in the header
+ * @param {Element} header The header block element
+ */
+function instrumentTrackingEvents(header) {
+  header.querySelectorAll('a')
+    .forEach((anchor) => {
+      anchor.addEventListener('click', (e) => {
+        const linkText = (e.target.textContent || '').trim();
+        const linkUrl = e.target.href;
+
+        // track Get Started anchor click
+        if (linkText === 'Get Started') {
+          trackGTMEvent('cta_click', {
+            cta_location: 'header_cta',
+            link_text: linkText,
+            link_url: linkUrl,
+          });
+        }
+      });
+    });
+}
 
 export default async function decorate(block) {
   let baseHeaderUrl = '/fragments/us/header-paid';
@@ -21,4 +45,5 @@ export default async function decorate(block) {
   changeDomain(block);
   addCanadaToLinks(block);
   decorateLinks(block);
+  instrumentTrackingEvents(headerContainer);
 }
