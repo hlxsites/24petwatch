aaec9fa90d2c67f26f8af3ce065c2715887e086f
hero-dokane <46008504+hero-dokane@users.noreply.github.com>
2024-10-03 14:48:23 -0400

    24PW: New Template - Paid Blog  (#219)
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page
    
    * WIP on feature/paid-blog-landing-page

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -9,6 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+/* eslint-disable max-classes-per-file */
 
 /**
  * log RUM if part of the sample.
@@ -155,6 +156,22 @@ export function toClassName(name) {
  */
 export function toCamelCase(name) {
   return toClassName(name).replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+}
+
+/**
+ * Gets all the metadata elements that are in the given scope.
+ * @param {String} scope The scope/prefix for the metadata
+ * @returns an array of HTMLElement nodes that match the given scope
+ */
+export function getAllMetadata(scope) {
+  return [...document.head.querySelectorAll(`meta[property^="${scope}:"],meta[name^="${scope}-"]`)]
+    .reduce((res, meta) => {
+      const id = toClassName(meta.name
+        ? meta.name.substring(scope.length + 1)
+        : meta.getAttribute('property').split(':')[1]);
+      res[id] = meta.getAttribute('content');
+      return res;
+    }, {});
 }
 
 const ICONS_CACHE = {};
@@ -423,6 +440,36 @@ export function buildBlock(blockName, content) {
 }
 
 /**
+ * Dynamically loads JS and CSS resources
+ * @param {string} name The name of the module
+ * @param {string} jsPath The path to the JS file
+ * @param {string} cssPath The path to the CSS file
+ * @param  {...any} args Arguments to pass to the module
+ */
+async function loadModule(name, jsPath, cssPath, ...args) {
+  const cssLoaded = cssPath ? loadCSS(cssPath) : Promise.resolve();
+  const decorationComplete = jsPath
+    ? new Promise((resolve) => {
+      (async () => {
+        let mod;
+        try {
+          mod = await import(jsPath);
+          if (mod.default) {
+            await mod.default.apply(null, args);
+          }
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.log(`failed to load module for ${name}`, error);
+        }
+        resolve(mod);
+      })();
+    })
+    : Promise.resolve();
+  return Promise.all([cssLoaded, decorationComplete])
+    .then(([, api]) => api);
+}
+
+/**
  * Loads JS and CSS for a block.
  * @param {Element} block The block element
  */
@@ -572,11 +619,25 @@ export function decorateButtons(element) {
     a.title = a.title || a.textContent;
     if (a.href !== a.textContent) {
       const up = a.parentElement;
+      const down = a.firstElementChild;
       const twoup = a.parentElement.parentElement;
       if (!a.querySelector('img')) {
+        if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV') && twoup.parentElement.className !== 'mega-nav' && twoup.parentElement.parentElement.className !== 'login') {
+          a.className = 'button'; // default
+          up.classList.add('button-container');
+          if (a.childElementCount === 1 && down.tagName === 'STRONG') {
+            a.classList.add('cta');
+            a.innerHTML = down.innerHTML;
+          } else if (a.childElementCount === 1 && down.tagName === 'EM') {
+            a.classList.add('secondary');
+            a.innerHTML = down.innerHTML;
+          } else {
+            a.classList.add('primary');
+          }
+        }
         if (up.childNodes.length === 1 && up.tagName === 'STRONG'
           && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-          a.className = 'button primary';
+          a.className = 'button cta';
           twoup.classList.add('button-container');
         }
         if (up.childNodes.length === 1 && up.tagName === 'EM'
@@ -586,6 +647,12 @@ export function decorateButtons(element) {
         }
       }
     }
+  });
+  // If we have several primary buttons after one other
+  // then just add them to the same container
+  document.querySelectorAll('.button-container + .button-container').forEach((c) => {
+    c.previousElementSibling.append(c.firstElementChild);
+    c.remove();
   });
 }
 
@@ -638,7 +705,8 @@ export async function waitForLCP(lcpBlocks) {
  */
 export function loadHeader(header) {
   let headerBlock = buildBlock('header', '');
-  if (document.body.classList.contains('paid')) {
+
+  if (document.body.className.includes('paid')) {
     // Load special header for paid section of the website
     headerBlock = buildBlock('header-paid', '');
   } else if (document.body.classList.contains('only-logo')) {
@@ -656,15 +724,145 @@ export function loadHeader(header) {
  */
 export function loadFooter(footer) {
   let footerBlock = '';
-  if (!document.body.classList.contains('paid')) {
+  if (!document.body.className.includes('paid')) {
     footerBlock = buildBlock('footer', '');
-  } else if (document.body.classList.contains('paid')) {
+  } else if (document.body.className.includes('paid')) {
     // Load special footer for paid section of the website
     footerBlock = buildBlock('footer-paid', '');
   }
   footer.append(footerBlock);
   decorateBlock(footerBlock);
   return loadBlock(footerBlock);
+}
+
+/**
+ * Defines the execution context for the plugins.
+ * @type {object}
+ */
+export const executionContext = {
+  createOptimizedPicture,
+  getAllMetadata,
+  getMetadata,
+  decorateBlock,
+  decorateButtons,
+  decorateIcons,
+  loadBlock,
+  loadCSS,
+  loadScript,
+  sampleRUM,
+  toCamelCase,
+  toClassName,
+};
+
+/**
+ * Parses the plugin parameters.
+ * @param {string} id The plugin ID
+ * @param {string|object} config The plugin configuration
+ * @returns {object} The parsed plugin parameters
+ */
+function parsePluginParams(id, config) {
+  const pluginId = !config
+    ? id.split('/').splice(id.endsWith('/') ? -2 : -1, 1)[0].replace(/\.js/, '')
+    : id;
+  const pluginConfig = {
+    load: 'eager',
+    ...(typeof config === 'string' || !config
+      ? { url: (config || id).replace(/\/$/, '') }
+      : config),
+  };
+  pluginConfig.options ||= {};
+  return { id: pluginId, config: pluginConfig };
+}
+
+/**
+ * Plugins Registry - manage, load, and execute plugins
+ */
+class PluginsRegistry {
+  #plugins;
+
+  constructor() {
+    this.#plugins = new Map();
+  }
+
+  // Register a new plugin
+  add(id, config) {
+    const { id: pluginId, config: pluginConfig } = parsePluginParams(id, config);
+    this.#plugins.set(pluginId, pluginConfig);
+  }
+
+  // Get the plugin
+  get(id) { return this.#plugins.get(id); }
+
+  // Check if the plugin exists
+  includes(id) { return !!this.#plugins.has(id); }
+
+  // Load all plugins that are referenced by URL, and updated their configuration with the
+  // actual API they expose
+  async load(phase) {
+    [...this.#plugins.entries()]
+      .filter(([, plugin]) => plugin.condition
+      && !plugin.condition(document, plugin.options, executionContext))
+      .map(([id]) => this.#plugins.delete(id));
+    return Promise.all([...this.#plugins.entries()]
+      // Filter plugins that don't match the execution conditions
+      .filter(([, plugin]) => (
+        (!plugin.condition || plugin.condition(document, plugin.options, executionContext))
+        && phase === plugin.load && plugin.url
+      ))
+      .map(async ([key, plugin]) => {
+        try {
+          // If the plugin has a default export, it will be executed immediately
+          const pluginApi = (await loadModule(
+            key,
+            !plugin.url.endsWith('.js') ? `${plugin.url}/${key}.js` : plugin.url,
+            !plugin.url.endsWith('.js') ? `${plugin.url}/${key}.css` : null,
+            document,
+            plugin.options,
+            executionContext,
+          )) || {};
+          this.#plugins.set(key, { ...plugin, ...pluginApi });
+        } catch (err) {
+          // eslint-disable-next-line no-console
+          console.error('Could not load specified plugin', key);
+        }
+      }));
+  }
+
+  // Run a specific phase in the plugin
+  async run(phase) {
+    return [...this.#plugins.values()]
+      .reduce((promise, plugin) => ( // Using reduce to execute plugins sequencially
+        plugin[phase] && (!plugin.condition
+            || plugin.condition(document, plugin.options, executionContext))
+          ? promise.then(() => plugin[phase](document, plugin.options, executionContext))
+          : promise
+      ), Promise.resolve());
+  }
+}
+
+/**
+ * Templates Registry - manage templates
+ */
+class TemplatesRegistry {
+  // Register a new template
+  // eslint-disable-next-line class-methods-use-this
+  add(id, url) {
+    if (Array.isArray(id)) {
+      id.forEach((i) => window.hlx.templates.add(i));
+      return;
+    }
+    const { id: templateId, config: templateConfig } = parsePluginParams(id, url);
+    templateConfig.condition = () => toClassName(getMetadata('template')) === templateId;
+    window.hlx.plugins.add(templateId, templateConfig);
+  }
+
+  // Get the template
+  // eslint-disable-next-line class-methods-use-this
+  get(id) { return window.hlx.plugins.get(id); }
+
+  // Check if the template exists
+  // eslint-disable-next-line class-methods-use-this
+  includes(id) { return window.hlx.plugins.includes(id); }
 }
 
 /**
@@ -675,6 +873,8 @@ export function setup() {
   window.hlx.RUM_MASK_URL = 'full';
   window.hlx.codeBasePath = '';
   window.hlx.lighthouse = new URLSearchParams(window.location.search).get('lighthouse') === 'on';
+  window.hlx.plugins = new PluginsRegistry();
+  window.hlx.templates = new TemplatesRegistry();
 
   const scriptEl = document.querySelector('script[src$="/scripts/scripts.js"]');
   if (scriptEl) {
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -34,6 +34,43 @@ export function jsx(html, ...args) {
   return html.slice(1).reduce((str, elem, i) => str + args[i] + elem, html[0]);
 }
 
+window.hlx.templates.add([
+  '/templates/paid-blog-page',
+]);
+
+/**
+ * A generic helper function that checks if we are in a mobile context based on viewport size
+ * @returns true if we are on a mobile, and false otherwise
+ */
+export function isMobile() {
+  return window.innerWidth < 600;
+}
+
+/**
+ * A generic helper function that checks if we are in a tablet context based on viewport size
+ * @returns true if we are on a tablet, and false otherwise
+ */
+export function isTablet() {
+  return window.innerWidth < 1024;
+}
+
+/**
+ * Gets the value of a placeholder.
+ * @param {string} key The key of the placeholder to retrieve
+ * @param {Object} options The template strings to use
+ * @returns the desired placeholder string, or throws an error if not found
+ */
+export function getPlaceholder(key, options = {}) {
+  if (!window.placeholders) {
+    throw new Error('Please load placeholders first using "fetchPlaceholders".');
+  }
+  const placeholders = window.placeholders[window.hlx.contentBasePath || 'default'];
+  if (!placeholders[key]) {
+    throw new Error(`Placeholder "${key}" not found`);
+  }
+  return Object.entries(options).reduce((str, [k, v]) => str.replace(`{{${k}}}`, v), placeholders[key]);
+}
+
 /**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
@@ -204,6 +241,9 @@ export function loadScript(url, attrs, callback) {
 async function loadEager(doc) {
   document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
+
+  await window.hlx.plugins.run('loadEager');
+
   const main = doc.querySelector('main');
   if (main) {
     createInlineScript(document, document.body, getAlloyInitScript(), 'text/javascript');
@@ -274,7 +314,7 @@ function instrumentTrackingEvents(main) {
             trackCTAEvent(ctaLocation);
 
           // track .button cta clicks for paid pages
-          } else if (body.classList.contains('paid')) {
+          } else if (body.className.includes('paid')) {
             if (e.target.closest('.hero-paid-membership')) {
               ctaLocation = 'hero_cta';
             } else if (e.target.closest('.lifetime-paid-membership')) {
@@ -287,11 +327,13 @@ function instrumentTrackingEvents(main) {
               ctaLocation = 'lpm_plus_cta';
             } else if (linkUrl.includes('annual-protection-membership')) {
               ctaLocation = 'annual_cta';
+            } else if (e.target.closest('.callout-get-a-quote1')) {
+              ctaLocation = 'body_1_cta';
+            } else if (e.target.closest('.callout-get-a-quote2')) {
+              ctaLocation = 'body_2_cta';
             }
             trackCTAEvent(ctaLocation);
             return;
-
-          // track clicks on every other anchor with a button class
           } else {
             trackCTAEvent(null);
             return;
@@ -315,7 +357,6 @@ function instrumentTrackingEvents(main) {
           if (e.target.closest('.faq-paid-membership')) {
             ctaLocation = 'faq_cta';
           }
-
           trackCTAEvent(ctaLocation);
         }
       });
@@ -347,6 +388,7 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
+  await window.hlx.plugins.run('loadLazy');
   loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));
 
@@ -368,13 +410,19 @@ async function loadLazy(doc) {
  * without impacting the user experience.
  */
 function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
   // load anything that can be postponed to the latest here
+  window.setTimeout(() => {
+    window.hlx.plugins.load('delayed');
+    window.hlx.plugins.run('loadDelayed');
+    // eslint-disable-next-line import/no-cycle
+    return import('./delayed.js');
+  }, 3000);
 }
 
 async function loadPage() {
+  await window.hlx.plugins.load('eager');
   await loadEager(document);
+  await window.hlx.plugins.load('lazy');
   await loadLazy(document);
   loadDelayed();
 }
