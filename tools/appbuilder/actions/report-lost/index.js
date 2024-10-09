b192acfcfbdd61f0abea5681c40a0ff54b07c893
Felix Delval <fdelval@adobe.com>
2024-10-09 18:23:58 +0200

    Adding the appbuilder proxy code

---
+++ tools/appbuilder/actions/report-lost/index.js
@@ -0,0 +1,163 @@
+/*
+* <license header>
+*/
+
+/**
+ * This is a sample action showcasing how to access an external API
+ *
+ * Note:
+ * You might want to disable authentication and authorization checks against Adobe Identity Management System for a generic action. In that case:
+ *   - Remove the require-adobe-auth annotation for this action in the manifest.yml of your application
+ *   - Remove the Authorization header from the array passed in checkMissingRequestInputs
+ *   - The two steps above imply that every client knowing the URL to this deployed action will be able to invoke it without any authentication and authorization checks against Adobe Identity Management System
+ *   - Make sure to validate these changes against your security requirements before deploying the action
+ */
+
+const fetch = require('node-fetch')
+const { Core } = require('@adobe/aio-sdk')
+const { errorResponse, getBearerToken, stringParameters, checkMissingRequestInputs } = require('../utils')
+
+// main function that will be executed by Adobe I/O Runtime
+async function main(params) {
+  // create a Logger
+  const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' });
+  // 24PW PRD credentials
+  const captchaSecret = '6LdpPxIqAAAAAOBI1_Bj5BJUv5luwoMBGM06WtsL';
+
+  try {
+    // 'info' is the default level if not set
+    logger.info('Calling the main action');
+
+    // log parameters, only if params.LOG_LEVEL === 'debug'
+    logger.error(stringParameters(params));
+
+    // check for missing request input parameters and headers
+    const requiredParams = [
+      'chipNumber',
+      'petName',
+      'reportDate',
+      'street',
+      'city',
+      'state',
+      'country',
+      'reporterName',
+      'reporterPhone',
+      'reporterEmail',
+      'partnerID',
+      'isFound',
+      'token',
+    ];
+    const requiredHeaders = [];
+    const errorMessage = checkMissingRequestInputs(params, requiredParams, requiredHeaders)
+    if (errorMessage) {
+      // return and log client errors
+      return errorResponse(400, errorMessage, logger);
+    }
+
+    const {
+      chipNumber,
+      petName,
+      reportDate,
+      street,
+      city,
+      state,
+      country,
+      reporterName,
+      reporterPhone,
+      reporterEmail,
+      notes,
+      partnerID,
+      isFound,
+      reportSystemName,
+      reporterPhoneAlt,
+      recoveryCaseId,
+      reportedBy,
+      contactConsent,
+      reporterIp,
+      resolution,
+      internalViewOnly,
+      enterbyOrgnizationId,
+      enterByUserName,
+      token,
+    } = params;
+
+    // TODO: Remove payload object once the approach is approved
+    // Wrapping the body in another object with payload key to match the expected format of the final endpoint
+    const body = {
+      payload: {
+        chipNumber,
+        petName,
+        reportDate,
+        street,
+        city,
+        state,
+        country,
+        reporterName,
+        reporterPhone,
+        reporterEmail,
+        notes,
+        partnerID,
+        isFound,
+        reportSystemName,
+        reporterPhoneAlt,
+        recoveryCaseId,
+        reportedBy,
+        contactConsent,
+        reporterIp,
+        resolution,
+        internalViewOnly,
+        enterbyOrgnizationId,
+        enterByUserName,
+      }
+    };
+
+    logger.info('TEMP: final request body !!!:', JSON.stringify(body))
+
+    // Validate Captcha
+    const captchaResponse = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+      method: 'POST',
+      body: new URLSearchParams({ secret: captchaSecret, response: token }),
+    });
+    const data = await captchaResponse.json();
+    logger.debug('reCAPTCHA response:', data);
+
+    if (!captchaResponse.ok || !data.success) {
+      return errorResponse(400, 'Captcha Invalid', logger);
+    }
+
+    // Send body to final endpoint
+    // TODO Finalize the endpoint
+    const finalEndpoint = 'https://348665-24petwatch-stage.adobeioruntime.net/api/v1/web/24petwatch-appbuilder/proxy-pethealth-services/Report/LostPet/SubmitReport';
+    // const finalEndpoint = 'https://www.24petwatch.com/content/24petwatch/us/en/lost-pet-protection/report-lost-found-pet/_jcr_content.api.json/api/v1/Report/LostPet/SubmitReport';
+    const finalResponse = await fetch(finalEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!finalResponse.ok) {
+      const responseStatus = finalResponse.status;
+      const responseBody = await finalResponse.json();
+      logger.debug(responseStatus, responseBody);
+
+      return errorResponse(500, `Contact Us API error occured: ${responseStatus} ${JSON.stringify(responseBody)}`, logger);
+    }
+
+    return {
+      statusCode: 200,
+      body: {
+        message: "Success",
+      }
+    };
+
+  } catch (error) {
+    // log any server errors
+    logger.error(error);
+    // return with 500
+    return errorResponse(500, 'server error', logger);
+  }
+}
+
+exports.main = main
