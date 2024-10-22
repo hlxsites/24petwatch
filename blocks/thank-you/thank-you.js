d6121ee664e654f1ab48e9984fee77ce871af781
pgilmore-phi <paul.gilmore@pethealthinc.com>
2024-10-22 16:58:29 -0400

    Updating purchase event  DL with new requirements

--- blocks/thank-you/thank-you.js
+++ blocks/thank-you/thank-you.js
@@ -100,8 +100,11 @@ export default async function decorate() {
   const urlParams = new URLSearchParams(window.location.search);
   const paymentProcessorId = urlParams.get('PaymentProcessorCustomerId');
   const data = await getPaymentCustomerIDFromUUID(paymentProcessorId);
-  let getOwnerDetails = await getOwner(data.paymentPortalCustomerId);
   const currencyValue = isCanada ? CURRENCY_CANADA : CURRENCY_US;
+  const productTypes = [];
+  const dlItems = [];
+  let getOwnerDetails = await getOwner(data.paymentPortalCustomerId);
+  let totalShipping = 0;
 
   await putUpdateOwnerSaleStatus(getOwnerDetails.id);
 
@@ -117,7 +120,7 @@ export default async function decorate() {
 
   const h1 = document.querySelector('h1');
   const {
-    cartFlow, firstName, lastName, nonInsPromoCode,
+    firstName, lastName,
   } = getOwnerDetails;
   const { petSummaries } = getPurchaseSummaryDetails;
   const contentColumn = document.querySelector('.thank-you-purchase .columns > div:nth-child(1) > div');
@@ -142,6 +145,22 @@ export default async function decorate() {
     ul.innerHTML += membershipItem;
 
     contentColumn.appendChild(ul);
+
+    // update analyics values
+    totalShipping += pet.nonInsurancePetSummary.shipping;
+    productTypes.push(pet.nonInsurancePetSummary.membership.itemName);
+    // push each item object to items array
+    dlItems.push({
+      item_name: pet.nonInsurancePetSummary.membership.itemName,
+      currency: currencyValue,
+      discount: pet.nonInsurancePetSummary.discount,
+      item_category: 'membership', // membership
+      item_variant: '', // okay to be left empty
+      price: pet.nonInsurancePetSummary.amount,
+      quantity: pet.nonInsurancePetSummary.membership.quantity,
+      microchip_number: pet.microChipNumber,
+      product_type: pet.nonInsurancePetSummary.membership.itemName,
+    });
   });
 
   // build sub-total, tax, and total of items purchased
@@ -169,18 +188,15 @@ export default async function decorate() {
 
   const trackingData = {
     ecommerce: {
-      microchip_number: petSummaries[0].microChipNumber,
-      product_type: petSummaries[0].membershipName,
       transaction_id: externalTransactionID,
       affiliation: '24petwatch',
-      tax: summary.salesTaxes,
-      payment_type: paymentMethod,
       value: summary.totalDueToday,
+      tax: summary.salesTaxes,
+      shipping: totalShipping ? totalShipping.toFixed(2) : '0.00',
       currency: currencyValue,
-      shipping: petSummaries[0].nonInsurancePetSummary.shipping,
-      coupon: nonInsPromoCode,
-      flow: cartFlow,
-      customerid: getOwnerDetails.id,
+      payment_type: paymentMethod,
+      product_type: productTypes.join(', '),
+      items: dlItems,
     },
   };
 
